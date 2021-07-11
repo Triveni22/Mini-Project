b3695f11bff3c4e9e69d6e9a4502cea1f5966814
Triveni22 <85540441+Triveni22@users.noreply.github.com>
2021-07-11 19:08:11 +0530

    Add files via upload

---
+++ 3.Implementation/func.h
@@ -0,0 +1,51 @@
+
+#define __FUN_H__
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<windows.h>
+void gotoxy (int x, int y)
+{
+
+}
+void SetColor(int ForgC)
+{
+
+}
+void ClearColor()
+{
+
+}
+void ClearConsoleToColors(int ForgC, int BackC)
+{
+
+}
+void SetColorAndBackground(int ForgC, int BackC)
+{
+
+}
+void increase_month(int *mm,  int *yy)
+{
+
+}
+void decrease_month(int *mm,  int *yy)
+{
+
+}
+void print_date(int mm, int yy)
+{
+
+}
+void printMonth(int mon,int year,int x,int y)
+{
+
+}
+void AddNote()
+{
+
+}
+void showNote(int mm)
+{
+
+}
