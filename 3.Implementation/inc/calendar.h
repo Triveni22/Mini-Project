e514bf5c38020e718acd069b6c6af5544e4ba2d5
Triveni22 <85540441+Triveni22@users.noreply.github.com>
2021-07-13 15:59:41 +0530

    Create calendar.h

---
+++ 3.Implementation/inc/calendar.h
@@ -0,0 +1,49 @@
+#ifndef __calendar_H__
+#define __calendar_H__
+#include<stdio.h>
+#include<conio.h>
+#include<windows.h>
+void gotoxy (int x, int y)
+{
+
+}
+void SetColor(int ForgC)
+{
+
+}
+void ClearColor()
+{
+
+}
+void ClearConsoleToColors(int ForgC, int BackC)
+{
+
+}
+void SetColorAndBackground(int ForgC, int BackC)
+{
+
+}
+void increase_month(int *mm,  int *yy)
+{
+
+}
+void decrease_month(int *mm,  int *yy)
+{
+
+}
+void print_date(int mm, int yy)
+{
+
+}
+void printMonth(int mon,int year,int x,int y)
+{
+
+}
+void AddNote()
+{
+
+}
+void showNote(int mm)
+{
+
+}
