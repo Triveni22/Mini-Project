dbaab9506c737f5a6981066a74429e4d2f5c98bc
Triveni22 <85540441+Triveni22@users.noreply.github.com>
2021-07-13 11:17:20 +0530

    Update calendar.c

--- 3.Implementation/calendar.c
+++ 3.Implementation/calendar.c
@@ -1,10 +1,6 @@
 #include<stdio.h>
 #include<conio.h>
 #include<windows.h>
-
-/**********************************************
-    
-***********************************************/
 struct Date{
     int dd;
     int mm;
@@ -28,18 +24,18 @@ void gotoxy (int x, int y)
         SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), xy);
 }
 
-//This will set the forground color for printing in a console window.
+
 void SetColor(int ForgC)
 {
      WORD wColor;
-     //We will need this handle to get the current background attribute
+    
      HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
      CONSOLE_SCREEN_BUFFER_INFO csbi;
 
-     //We use csbi for the wAttributes word.
+  
      if(GetConsoleScreenBufferInfo(hStdOut, &csbi))
      {
-        //Mask out all but the background attribute, and add in the forgournd color
+       
           wColor = (csbi.wAttributes & 0xF0) + (ForgC & 0x0F);
           SetConsoleTextAttribute(hStdOut, wColor);
      }
@@ -53,27 +49,23 @@ void ClearColor(){
 void ClearConsoleToColors(int ForgC, int BackC)
 {
      WORD wColor = ((BackC & 0x0F) << 4) + (ForgC & 0x0F);
-     //Get the handle to the current output buffer...
+  
      HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
-     //This is used to reset the carat/cursor to the top left.
+
      COORD coord = {0, 0};
-     //A return value... indicating how many chars were written
-     //   not used but we need to capture this since it will be
-     //   written anyway (passing NULL causes an access violation).
+    
      DWORD count;
 
-     //This is a structure containing all of the console info
-     // it is used here to find the size of the console.
      CONSOLE_SCREEN_BUFFER_INFO csbi;
-     //Here we will set the current color
+ 
      SetConsoleTextAttribute(hStdOut, wColor);
      if(GetConsoleScreenBufferInfo(hStdOut, &csbi))
      {
-          //This fills the buffer with a given character (in this case 32=space).
+         
           FillConsoleOutputCharacter(hStdOut, (TCHAR) 32, csbi.dwSize.X * csbi.dwSize.Y, coord, &count);
 
           FillConsoleOutputAttribute(hStdOut, csbi.wAttributes, csbi.dwSize.X * csbi.dwSize.Y, coord, &count );
-          //This will set our cursor position for the next print statement.
+     
           SetConsoleCursorPosition(hStdOut, coord);
      }
      return;
@@ -385,7 +377,7 @@ int main(){
         printf("4. EXIT\n");
         printf("ENTER YOUR CHOICE : ");
         scanf("%d",&choice);
-        system("cls");
+       
         switch(choice){
             case 1:
                 printf("Enter date (DD MM YYYY) : ");
@@ -397,21 +389,21 @@ int main(){
             case 2 :
                 printf("Enter month and year (MM YYYY) : ");
                 scanf("%d %d",&date.mm,&date.yy);
-                system("cls");
+               
                 while(ch!='q'){
                     printMonth(date.mm,date.yy,20,5);
                     ch = getch();
                     if(ch == 'n'){
                         increase_month(&date.mm,&date.yy);
-                        system("cls");
+                     
                         printMonth(date.mm,date.yy,20,5);
                     }else if(ch == 'p'){
                         decrease_month(&date.mm,&date.yy);
-                        system("cls");
+                    
                         printMonth(date.mm,date.yy,20,5);
                     }else if(ch == 's'){
                         showNote(date.mm);
-                        system("cls");
+                   
                     }
                 }
                 break;
